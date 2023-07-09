715543b7b5ec81ec745caa7fb6c345131a0ee278
Sudeep <sdreple@gmail.com>
2023-07-09 09:14:13 +0545

    project Started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Sudeep Fullel
+      <div><button>Click me</button></div>
+    </div>
+  );
+}
+
+export default App;
