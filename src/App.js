68b3697884ffa3f35bc02db4f6d6b51e4a37bb23
Sudeep <sdreple@gmail.com>
2023-07-09 09:14:35 +0545

    project started

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const clickFunction = () => {
+    alert("you clicked me!")
+  };
+
   return (
     <div className="App">
       Sudeep Fullel
-      <div><button>Click me</button></div>
+      <div><button onClick={clickFunction}>Click me</button></div>
     </div>
   );
 }
